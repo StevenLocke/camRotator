c81b42f065758dc43c92b99699e98b1a1e6ac60b
Steven Locke <slocke@pivotal.io>
2019-11-11 17:57:13 -0800

    Request cams over https

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function randomCam() {
 	const entries = Object.entries(cams)
 	const randomIndex = Math.floor(Math.random() * entries.length);
 	const [title, channel] = entries[randomIndex]
-	const camSource = `http://anything.player.camzonecdn.com/v1.2/CamzoneStreamPlayer?iframe=yes&channel=${channel}&muted=yes&mutebutton=no&czlogourl=&toolbar=always&backgroundcolor=000000&toolbarbgcolor=ffffff&toolbaralpha=100&toolbartextcolor=000000&epostcard=no&toolbartype=new&toolbarposition=topright`;
+	const camSource = `https://anything.player.camzonecdn.com/v1.2/CamzoneStreamPlayer?iframe=yes&channel=${channel}&muted=yes&mutebutton=no&czlogourl=&toolbar=always&backgroundcolor=000000&toolbarbgcolor=ffffff&toolbaralpha=100&toolbartextcolor=000000&epostcard=no&toolbartype=new&toolbarposition=topright`;
 	document.getElementById("frame").src = camSource;
 	document.title = `${title} cam`;
 }
